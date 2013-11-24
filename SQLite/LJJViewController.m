53f633210bc8f52c0c25ad7095956abf795648fd
Jun <jun@JundeiMac.local>
2013-11-24 21:33:34 +0800

    添加一个按钮 实现sqlLite 添加一条数据 添加3个文本框 将数据添加

--- SQLite/LJJViewController.m
+++ SQLite/LJJViewController.m
@@ -11,6 +11,9 @@
 #import "Person.h"
 
 @interface LJJViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *name;
+@property (weak, nonatomic) IBOutlet UITextField *age;
+@property (weak, nonatomic) IBOutlet UITextField *height;
 
 @end
 
@@ -30,6 +33,14 @@
 #pragma mark 常见表格
 -(IBAction)createTable {
     [[LJSQLite sharedLJSQLite] createTable:[Person class] autoincrement:YES];
+}
+#pragma mark 添加一条数据
+- (IBAction)addData {
+    Person * p = [[Person alloc]init];
+    p.name = _name.text;
+    p.age = [_age.text integerValue];
+    p.height = [_height.text doubleValue];
+    [[LJSQLite sharedLJSQLite]addObject:p];
 }
 
 @end
