57603df8c1c9ceb83317c81e4427e97b5965300d
Jun <jun@JundeiMac.local>
2013-11-25 22:37:23 +0800

    添加 	遍历成员变量名和对应数据类型名 	获取当前对象的成员变量与对应的参数

--- SQLite/SQLite/NSObject+LJSQLite.h
+++ SQLite/SQLite/NSObject+LJSQLite.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void(^IvarNamesUsingBlock)(NSString * name,NSString * type,int idx,BOOL *stop);
+
 @interface NSObject (LJSQLite)
 /**
  *  获取当前对象中的主键值
@@ -16,4 +18,18 @@
  *  @return 返回对象主键值
  */
 - (id)primaryKey;
+
+/**
+ *  遍历类的所有成员变量
+ *
+ *  @param block 遍历block ，成员遍历名、数据类型名、位置、是否暂停，默认为no
+ */
++ (void)enumerateIvarNamesUsingBlock:(IvarNamesUsingBlock)block;
+
+/**
+ *  获取当前对象的成员变量名与对应的参数
+ *
+ *  @return 返回字典 key成员变量名 value参数
+ */
+- (NSDictionary *)params;
 @end
--- SQLite/SQLite/NSObject+LJSQLite.m
+++ SQLite/SQLite/NSObject+LJSQLite.m
@@ -18,25 +18,77 @@
  */
 - (id)primaryKey {
     Class c = [self class];
-    NSValue * value = nil;
+    __block NSValue * value = nil;
     
-    unsigned int outCount = 0;
-    Ivar *ivars = class_copyIvarList(c, &outCount);
+//    unsigned int outCount = 0;
+//    Ivar *ivars = class_copyIvarList(c, &outCount);
+//
+//    for (int i = 0; i<outCount; i++){
+//        Ivar ivar = ivars[i];
+//        // 1.属性名
+//        NSString *name = [NSMutableString stringWithUTF8String:ivar_getName(ivar)] ;
+//        
+//        //包含id为主键
+//        NSRange range = [[name lowercaseString] rangeOfString:@"id"];
+//        if ((range.length + range.location)== name.length){
+//            //去下划线
+//            name = [name substringFromIndex:1];
+//            value = [name valueForKey:name];
+//            break;
+//        }
+//    }
     
-    for (int i = 0; i<outCount; i++){
-        Ivar ivar = ivars[i];
-        // 1.属性名
-        NSString *name = [NSMutableString stringWithUTF8String:ivar_getName(ivar)] ;
-        
+    [c enumerateIvarNamesUsingBlock:^(NSString *name, NSString *type, int idx, BOOL *stop) {
         //包含id为主键
         NSRange range = [[name lowercaseString] rangeOfString:@"id"];
         if ((range.length + range.location)== name.length){
             //去下划线
             name = [name substringFromIndex:1];
-            value = [name valueForKey:name];
+            value = [self valueForKey:name];
+            *stop = YES;
+        }
+    }];
+    
+    return value;
+}
+/**
+ *  遍历类的所有成员变量
+ *
+ *  @param block 遍历block ，成员遍历名、数据类型名、位置、是否暂停，默认为no
+ */
++ (void)enumerateIvarNamesUsingBlock:(IvarNamesUsingBlock)block {
+    unsigned int outCount = 0;
+    Ivar *ivars = class_copyIvarList(self, &outCount);
+    
+    for (int i = 0; i<outCount; i++){
+        Ivar ivar = ivars[i];
+        // 1.属性名
+        NSString *name = [NSMutableString stringWithUTF8String:ivar_getName(ivar)] ;
+        //2.数据类型
+        NSString *type = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivar)];
+        BOOL stop = NO;
+        if (block) {
+            block(name,type,i,&stop);
+        }
+        
+        if (stop) {
             break;
         }
+        
     }
-    return value;
+}
+
+
+- (NSDictionary *)params {
+    NSMutableDictionary * params = [NSMutableDictionary dictionary];
+    [[self class] enumerateIvarNamesUsingBlock:^(NSString *name, NSString *type, int idx, BOOL *stop) {
+        //去下划线
+        name = [name substringFromIndex:1];
+        NSValue * value = [self valueForKey:name];
+        if (value) {
+            [params setObject:value forKey:name];
+        }
+    }];
+    return params;
 }
 @end
