d94e851317e9abdd6d496b504e829bfd4abd75e9
Jun <jun@JundeiMac.local>
2013-11-25 10:36:11 +0800

    添加分类 新增删除新操作 对where条件判断操作

---
+++ SQLite/SQLite/LJSQLite+Where.h
@@ -0,0 +1,30 @@
+//
+//  LJSQLite+Where.h
+//  SQLite
+//
+//  Created by Jun on 13-11-25.
+//  Copyright (c) 2013年 Jun. All rights reserved.
+//
+
+#import "LJSQLite.h"
+
+
+
+@interface LJSQLite (Where)
+/**
+ *  删除对应数据表的对象数据
+ *
+ *  @param objClass     对象类
+ *  @param where        查找条件 key：成员变量名 value：查询数据  如：@"name" : @"adf"
+ *                  where name = @"adf" and ...;
+ */
+-(void)deleteObjects:(Class)objClass where:(NSDictionary *)where;
+
+/**
+ *  删除对应数据表的对象数据
+ *
+ *  @param objClass 对象类
+ *  @param whereStr 判断字符串  例如：@"name = 'bac' and age = 12 or height > 1.1 and height < 5"
+ */
+-(void)deleteObjects:(Class)objClass whereStr:(NSString *)whereStr;
+@end
---
+++ SQLite/SQLite/LJSQLite+Where.m
@@ -0,0 +1,64 @@
+//
+//  LJSQLite+Where.m
+//  SQLite
+//
+//  Created by Jun on 13-11-25.
+//  Copyright (c) 2013年 Jun. All rights reserved.
+//
+
+#import "LJSQLite+Where.h"
+#import <sqlite3.h>
+#import <objc/message.h>
+#import <objc/runtime.h>
+
+@implementation LJSQLite (Where)
+
+/**
+ *  删除对应数据表的对象数据
+ *
+ *  @param objClass     对象
+ *  @param where        查找条件 key：成员变量名 value：查询数据  如：@"name" : @"adf"
+ *                  where name = @"adf" and ...;
+ */
+-(void)deleteObjects:(Class)objClass where:(NSDictionary *)where {
+    //拼接where语句
+    NSMutableString * whereStr = [NSMutableString string];
+    
+    NSArray * array = [where allKeys];
+    
+    for (NSString * name in array) {
+        NSValue * value = where[name];
+        if ([value isKindOfClass:[NSString class]]) {
+            [whereStr appendFormat:@" %@ = '%@' and",name,value];
+        } else {
+            [whereStr appendFormat:@" %@ = %@ and",name,value];
+        }
+        
+    }
+    [whereStr deleteCharactersInRange:NSMakeRange(whereStr.length - 3, 3)];
+    
+    [self deleteObjects:objClass whereStr:whereStr];
+}
+/**
+ *  删除对应数据表的对象数据
+ *
+ *  @param objClass 对象类
+ *  @param whereStr 判断字符串  例如：@"name = 'bac' and age = 12 or height > 1.1 and height < 5"
+ */
+-(void)deleteObjects:(Class)objClass whereStr:(NSString *)whereStr {
+    //表名
+    NSString * tableName = kTableName(objClass);
+    NSMutableString * sql = [NSMutableString stringWithFormat:@"DELETE FROM %@ WHERE ",tableName];
+    
+    [sql appendString:whereStr];
+    
+    [sql appendString:@";"];
+    
+    [self execSQL:sql msg:@"删除数据"];
+}
+
+//-(NSArray *)allObjects:(Class)objClass where:(NSDictionary *)where {
+//
+//}
+
+@end
